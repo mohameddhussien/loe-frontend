5ba0bc518b092f72ef765f540cddc764f7dd5c9e
Abdo-Hussien <m.boody2004@gmail.com>
2024-02-03 19:23:37 +0200

    bus

--- components/booking/reservation/bus/Content.vue
+++ components/booking/reservation/bus/Content.vue
@@ -1,36 +1,52 @@
 <template>
-    <v-img src="../../../../public/Bus-Plan.png"></v-img>
-    <v-container>
-        <h1 class="text-3xl mb-4">Choose your Seats</h1>
-        <v-row>
-            <v-col v-for="(button, index) in 13" :key="index">
-        <button @click="changeColor(index)" class="color-button border">A</button>
+  <v-img src="../../../../public/Bus-Plan.png"></v-img>
+  <v-container>
+    <h1 class="text-3xl mb-4">Choose your Seats</h1>
+    <v-row v-for="(row, rowIndex) in buttons" :key="rowIndex">
+      <v-col v-for="(btnObj, btn_index) in row" :key="btn_index">
+        <v-btn @click="changeColor(btnObj)" :class="{
+          [`bg-${btnObj.bgColor}`]: true, 'color-button border': true,
+          'd-none': isButtonDisabled(rowIndex,btn_index)
+        }" :icon="btnObj.icon" />
       </v-col>
-        </v-row>
-       
-    </v-container>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
-import { ref } from 'vue';
 
-const color = ref('');
-const buttons = ref(Array(13).fill(0));
+const buttons = Array(4).fill(Array(13).fill({
+  bgColor: 'white',
+  color: '',
+  disabled: false,
+  label: '',
+  icon: ''
+}));
 
-const changeColor = (index) => {
-  color.value = '#64DD17';
-  buttons.value[index] += 1;
+const changeColor = (buttonObj) => {
+  buttonObj.bgColor = 'green-lighten-1'
+};
+
+const isButtonDisabled = (rowIndex, buttonIndex) => {
+  return (rowIndex === 2 && (buttonIndex === 6 || buttonIndex === 7)) ||
+    (rowIndex === 3 && (buttonIndex === 6 || buttonIndex === 7));
 };
 </script>
 
 <style scoped>
 .color-button {
-  border: 1px solid #000; /* Add your desired border style */
-  padding: 2px; /* Add padding for better appearance */
+  border: 3px;
+  border-radius: 5px;
+  /* Add your desired border style */
+  padding: 2px;
+  /* Add padding for better appearance */
   cursor: pointer;
 }
 
-.color-button.clicked {
-  background-color: #64DD17; /* Change background color when clicked */
+.color-button:hover{
+  background-color: green;
+}
+.bg-green-lighten-1 {
+  background-color: #64DD17 !important;
 }
 </style>
